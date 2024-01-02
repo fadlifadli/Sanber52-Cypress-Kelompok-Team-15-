670de8f2fbdfc626702b0c09f7e4cbbb1cc195df
Muhammad Azhari <Muhammad.Azhari@atibusinessgroup.com>
2024-01-02 16:13:49 +0700

    Create Account

---
+++ cypress/e2e/Sanber_Quiz3_week3/CreateAccount_Quiz_Week3.cy.js
@@ -0,0 +1,21 @@
+describe('My First Test Cypress', () => {
+  it('Create Account "Create"', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
+
+    // Click text button
+    cy.contains('Create an Account').click()
+
+    // Get an input, type into it 
+    cy.get('#firstname').type('SanberTeam15ok')
+    cy.get('#lastname').type('QA')
+    cy.get('#email_address').type('QA_Sanber@team15Okk.com')
+    cy.get('#password').type('SanberTeam15')
+    cy.get('#password-confirmation').type('SanberTeam15')
+
+    // action submit primary
+    cy.get('#form-validate > .actions-toolbar > div.primary> .action > span').click()
+
+    
+    // button[title='Create an Account'] > span
+  })
+})
---
+++ file2.js
@@ -0,0 +1,23 @@
+// var
+
+var buah = 'apel'
+buah = "jeruk"
+console.log(buah)
+
+// let
+// let hewan = Kambing
+// hewan = kucing
+// console.log(hewan)
+
+// cons
+const angka = 555
+// angka = 666
+console.log(angka)
+
+// function
+function pengurangan(a,b){
+    return a - b
+}
+
+let jumlah = pengurangan (10,2)
+console.log(jumlah)

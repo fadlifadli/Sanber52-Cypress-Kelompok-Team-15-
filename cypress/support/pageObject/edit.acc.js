a1b4d926e08a0130107ddb9f253870ba0ccbf232
rachmakiki <rachmakikirrm@gmail.com>
2024-01-03 09:23:11 +0700

    update edit account functionality

---
+++ cypress/support/pageObject/edit.acc.js
@@ -0,0 +1,10 @@
+class editAcc {
+    fname = '#firstname'
+    lname = '#lastname'
+    newEmail = '#email'
+    current = '#current-password'
+    new = '#password'
+    confirm = '#password-confirmation'
+    succesMsg = '.message-success'
+}
+export default new editAcc()

1532d9941665ab910b03a74dd2edf0c1064a9fc5
pramesta <pramestakharismak@gmail.com>
2024-01-04 10:06:09 +0700

    Edit Address by Pramesta

---
+++ cypress/support/pageObject/edit.add.js
@@ -0,0 +1,10 @@
+class editAdd {
+    fname = '#firstname'
+    lname = '#lastname'
+    newEmail = '#email'
+    current = '#current-password'
+    new = '#password'
+    confirm = '#password-confirmation'
+    succesMsg = '.message-success'
+}
+export default new editAdd()

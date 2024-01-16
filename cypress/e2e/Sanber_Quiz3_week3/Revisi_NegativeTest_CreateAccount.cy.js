430d2cbf2c1b65b4e2164f15e1ab153af0d629e3
M. Fadhli Azhari <fadli.azhari.elka@gmail.com>
2024-01-16 10:55:05 +0700

    Update Revisi_NegativeTest_CreateAccount.cy.js

--- cypress/e2e/Sanber_Quiz3_week3/Revisi_NegativeTest_CreateAccount.cy.js
+++ cypress/e2e/Sanber_Quiz3_week3/Revisi_NegativeTest_CreateAccount.cy.js
@@ -1,6 +1,5 @@
 context('Negative Test Case Create Account', () => {
   beforeEach(() => {
-      // cy.visit('https://magento.softwaretestingboard.com/')
       cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
       
     })
@@ -9,36 +8,27 @@ context('Negative Test Case Create Account', () => {
     it('this page true create  account ', () => {
       // this page true create account
       cy.get('.base')
-    // })
+    })
 
-    // it('No Input field FirstName', () => {
-    //   // input, type into it 
-    //   cy.get('#firstname').type()
-    // })
-
-    // it('No Input field lastname', () => {
-    //   // input, type into it 
-    //   cy.get('#lastname').type()
-    // })
-
-    // it('Wrong Input email_address', () => {
+    it('Wrong Input email_address', () => {
       // input, type into it 
-      cy.get('#email_address').type('1234')   
-    // })
+      cy.get('#email_address').type('1234')
+      cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+      cy.get('#email_address-error').should('be.visible')
+      cy.wait(500)   
+    })
 
-    // it('Wrong Input Password', () => {
+    it('Wrong Input Password', () => {
       // input, type into it 
-      cy.get('#password').type('123456')   
-    // })
+      cy.get('#password').type('123456')
+      cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+      cy.get('#password-error').should('be.visible')     
+    })
 
-    // it('Wrong Input password-confirmation', () => {
-      // input, type into it 
-      cy.get('#password-confirmation').type('123456')   
-    // })
 
-    // it('Click Button Submit', () => {
+    it('VALIDASI FIRST-NAME ERROR, LAST-NAME ERROR, Fail Email, password-error  ', () => {
       // action submit primary
-      cy.get('#form-validate > .actions-toolbar > div.primary> .action > span').click()
+      cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
     // })
 
     // it('VALIDASI FIRST-NAME ERROR', () => {
@@ -59,8 +49,8 @@ context('Negative Test Case Create Account', () => {
       cy.wait(500)
     // })
 
-    // it('VALIDASI Failed Login', () => {
-      // Verify Failed Login
+    // it('VALIDASI Pasword Error', () => {
+      // Verify Pasword Error
       cy.get('#password-error').should('be.visible')
       cy.wait(500)
  })
